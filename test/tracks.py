a86df476321656c2f849377bb896f39619a8c586
konec <>
2009-11-12 19:13:19 +0000

    fix incorrect order of calls while checking for a new ES+related cleanup

--- test/tracks.py
+++ test/tracks.py
@@ -1,9 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 process = cms.Process("Analysis")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100))
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(5))
 process.source = cms.Source("PoolSource", fileNames =  cms.untracked.vstring(
-  'file:SingleMu_5p00.root'
+  'file:SingleMu.root'
+#  'file:MinBias.root'
 #  'rfio:/castor/cern.ch/user/k/konec/cmssw31x/SingleMu_10m00.root' 
 #'/store/relval/CMSSW_3_1_0_pre4/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG/IDEAL_30X_v1/0003/3AA6EEA4-3B16-DE11-B35F-001617C3B654.root'
 ))
@@ -11,6 +12,7 @@ process.source = cms.Source("PoolSource", fileNames =  cms.untracked.vstring(
 
 # import of standard configurations
 process.load('Configuration/StandardSequences/Services_cff')
+process.load('Configuration/StandardSequences/MixingNoPileUp_cff')
 process.load('Configuration/StandardSequences/GeometryExtended_cff')
 process.load('Configuration/StandardSequences/MagneticField_38T_cff')
 process.load('Configuration/StandardSequences/RawToDigi_cff')
@@ -20,11 +22,13 @@ process.load('FWCore/MessageService/MessageLogger_cfi')
 
 # Other statements
 #process.GlobalTag.globaltag = 'IDEAL_31X::All'
-process.GlobalTag.globaltag = 'STARTUP31X_V1::All'
+#process.GlobalTag.globaltag = 'STARTUP31X_V1::All'
+process.GlobalTag.globaltag = 'MC_31X_V9::All'
 
 process.load("RecoTracker.Configuration.RecoTracker_cff")
 from RecoTracker.Configuration.RecoTracker_cff import *
 process.load('RecoLocalTracker/Configuration/RecoLocalTracker_cff')
+process.load("RecoPixelVertexing.PixelTrackFitting.PixelTracks_cff")
 
 process.load("RecoTracker.IterativeTracking.FirstStep_cff")
 from RecoTracker.IterativeTracking.FirstStep_cff import *
@@ -53,4 +57,5 @@ process.analysis = cms.EDFilter("TrackAnalysis",
      ptMinLeadingTrack = cms.double(0.05)
   )
 )
-process.p=cms.Path(process.RawToDigi*process.trackerlocalreco*process.offlineBeamSpot*process.globalPixelSeeds*newTrackCandidateMaker*preFilterZeroStepTracks*process.analysis)
+process.p=cms.Path(process.RawToDigi*process.trackerlocalreco*process.offlineBeamSpot*process.globalPixelSeeds*newTrackCandidateMaker*preFilterZeroStepTracks*process.analysis) 
+#process.p=cms.Path(process.offlineBeamSpot*process.globalPixelSeeds*newTrackCandidateMaker*preFilterZeroStepTracks*process.analysis)
