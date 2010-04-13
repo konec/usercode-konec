bab797d230a9eb21bbb1daaeb93588e93348e0be
konec <>
2010-04-13 13:29:02 +0000

    *** empty log message ***

--- test/rpcMonitor.py
+++ test/rpcMonitor.py
@@ -3,30 +3,30 @@ process = cms.Process("Analysis")
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1))
 process.source = cms.Source("PoolSource", fileNames =  cms.untracked.vstring( 
-'file:/disk00/work/data/run132440.muon/208A5482-4D3C-DF11-97EA-00E08178C0FB.root',
-'file:/disk00/work/data/run132440.muon/BC463F24-4A3C-DF11-998A-00E08178C085.root',
-'file:/disk00/work/data/run132440.muon/963F1CB1-443C-DF11-B5F1-003048673F1C.root',
-'file:/disk00/work/data/run132440.muon/86912F13-4C3C-DF11-9CEB-00E0817918BF.root'
-#'file:/disk00/work/data/run132440.tmf/run132440_158_167.root',
-#'file:/disk00/work/data/run132440.tmf/run132440_168_177.root',
-#'file:/disk00/work/data/run132440.tmf/run132440_178_187.root',
-#'file:/disk00/work/data/run132440.tmf/run132440_188_197.root',
-#'file:/disk00/work/data/run132440.tmf/run132440_198_207.root',
-#'file:/disk00/work/data/run132440.tmf/run132440_208_217.root',
-#'file:/disk00/work/data/run132440.tmf/run132440_218_227.root',
-#'file:/disk00/work/data/run132440.tmf/run132440_228_237.root',
-#'file:/disk00/work/data/run132440.tmf/run132440_238_247.root',
-#'file:/disk00/work/data/run132440.tmf/run132440_248_257.root',
-#'file:/disk00/work/data/run132440.tmf/run132440_258_267.root',
-#'file:/disk00/work/data/run132440.tmf/run132440_268_277.root',
-#'file:/disk00/work/data/run132440.tmf/run132440_278_287.root',
-#'file:/disk00/work/data/run132440.tmf/run132440_288_297.root',
-#'file:/disk00/work/data/run132440.tmf/run132440_298_307.root',
-#'file:/disk00/work/data/run132440.tmf/run132440_308_317.root',
-#'file:/disk00/work/data/run132440.tmf/run132440_318_327.root',
-#'file:/disk00/work/data/run132440.tmf/run132440_328_337.root',
-#'file:/disk00/work/data/run132440.tmf/run132440_338_347.root',
-#'file:/disk00/work/data/run132440.tmf/run132440_348_357.root' 
+#'file:/disk00/work/data/run132440.muon/208A5482-4D3C-DF11-97EA-00E08178C0FB.root',
+#'file:/disk00/work/data/run132440.muon/BC463F24-4A3C-DF11-998A-00E08178C085.root',
+#'file:/disk00/work/data/run132440.muon/963F1CB1-443C-DF11-B5F1-003048673F1C.root',
+#'file:/disk00/work/data/run132440.muon/86912F13-4C3C-DF11-9CEB-00E0817918BF.root'
+'file:/disk00/work/data/run132440.tmf/run132440_158_167.root',
+'file:/disk00/work/data/run132440.tmf/run132440_168_177.root',
+'file:/disk00/work/data/run132440.tmf/run132440_178_187.root',
+'file:/disk00/work/data/run132440.tmf/run132440_188_197.root',
+'file:/disk00/work/data/run132440.tmf/run132440_198_207.root',
+'file:/disk00/work/data/run132440.tmf/run132440_208_217.root',
+'file:/disk00/work/data/run132440.tmf/run132440_218_227.root',
+'file:/disk00/work/data/run132440.tmf/run132440_228_237.root',
+'file:/disk00/work/data/run132440.tmf/run132440_238_247.root',
+'file:/disk00/work/data/run132440.tmf/run132440_248_257.root',
+'file:/disk00/work/data/run132440.tmf/run132440_258_267.root',
+'file:/disk00/work/data/run132440.tmf/run132440_268_277.root',
+'file:/disk00/work/data/run132440.tmf/run132440_278_287.root',
+'file:/disk00/work/data/run132440.tmf/run132440_288_297.root',
+'file:/disk00/work/data/run132440.tmf/run132440_298_307.root',
+'file:/disk00/work/data/run132440.tmf/run132440_308_317.root',
+'file:/disk00/work/data/run132440.tmf/run132440_318_327.root',
+'file:/disk00/work/data/run132440.tmf/run132440_328_337.root',
+'file:/disk00/work/data/run132440.tmf/run132440_338_347.root',
+'file:/disk00/work/data/run132440.tmf/run132440_348_357.root' 
 ))
 
 
@@ -79,26 +79,12 @@ process.dqmEnv.subSystemFolder = 'R2DTEST'
 process.MessageLogger = cms.Service("MessageLogger",
     #debugModules = cms.untracked.vstring('pixelVertices'),
     #debugModules = cms.untracked.vstring('pixelTracks'),
-    #debugModules = cms.untracked.vstring('rpcunpacker'),
+    debugModules = cms.untracked.vstring('rpcunpacker','rpcMonitorLinkSynchro'),
     #debugModules = cms.untracked.vstring('*'),
-    debugModules = cms.untracked.vstring(''),
+    #debugModules = cms.untracked.vstring(''),
     destinations = cms.untracked.vstring('cout'),
-    cout = cms.untracked.PSet( threshold = cms.untracked.string('INFO'))
+    cout = cms.untracked.PSet( threshold = cms.untracked.string('DEBUG'))
 )
 
-process.p = cms.Path(
-     process.l1GtUnpack
-    *process.filter
-    *process.rpcunpacker
-    *process.rpcFEDIntegrity
-    *process.rpcMonitorRaw
-    *process.rpcMonitorLinkSynchro
-    *process.l1trpctf
-*process.dqmEnv*process.dqmSaver)
-
-#process.p=cms.Path(process.siPixelDigis)
-#process.p=cms.Path(process.rpcunpacker)
-#process.p=cms.Path(process.RawToDigi)
-#process.p=cms.Path(process.rpcunpacker*process.analysis)
-#process.p=cms.Path(process.RawToDigi*process.pixeltrackerlocalreco*process.pixelTracks*process.analysis)
-#process.p=cms.Path(process.siPixelDigis*process.pixeltrackerlocalreco*process.offlineBeamSpot*process.pixelTracks*process.pixelVertices*process.analysis)
+process.p = cms.Path( process.l1GtUnpack *process.filter *process.rpcunpacker *process.rpcFEDIntegrity 
+ *process.rpcMonitorRaw *process.rpcMonitorLinkSynchro*process.l1trpctf *process.dqmEnv*process.dqmSaver)
