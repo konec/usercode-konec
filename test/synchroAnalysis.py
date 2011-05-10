1aba406c7717c2f7c6e2cfbb6c6438cdb879e7ed
konec <>
2011-05-10 08:59:50 +0000

    *** empty log message ***

--- test/synchroAnalysis.py
+++ test/synchroAnalysis.py
@@ -4,33 +4,19 @@ process = cms.Process("Analysis")
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1))
 process.source = cms.Source("PoolSource", fileNames =  cms.untracked.vstring( 
-#'file:/disk00/work/data/run147647/E29BE225-32D4-DF11-BA24-001D09F24E39.root'),
-#'file:/disk00/work/data/run158335/502449EB-DE3B-E011-B733-0030487CD77E.root'),
-#'file:/disk00/work/data/run160413/983CBA22-C44D-E011-B124-00304879BAB2.root'),
-#'file:/disk00/work/data/run160413/F8D81AA3-BE4D-E011-8DB2-001D09F24399.root'),
-'file:/disk00/work/data/run160943/DAC0F150-1A53-E011-99B4-001D09F2514F.root'),
-#'file:/disk00/work/data/Commissioning-MuonDPG_skim-May27thSkim_v2/1E1A93A9-B96D-DF11-848B-003048D47A2E.root'),
-#'file:/disk00/work/data/Commissioning-MuonDPG_skim-May27thSkim_v2/B60EB18B-AA6D-DF11-9E3E-003048D476C6.root'),
-#'file:/disk00/work/data/Commissioning-MuonDPG_skim-May27thSkim_v2/96984B02-996D-DF11-BB4F-003048D460AE.root'),
-inputCommands=cms.untracked.vstring( 'keep *', 'drop *_hltL1GtObjectMap_*_*')
+  'file:/disk00/work/CMSSW_4_2_3.RPC/jobs_gen/SingleMu_2.root')
 )
 
 # import of standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
-#process.load('Configuration.StandardSequences.GeometryDB_cff')
 process.load('Configuration.StandardSequences.GeometryExtended_cff')
 process.load('Configuration.StandardSequences.MagneticField_38T_cff')
-#process.load('Configuration.StandardSequences.RawToDigi_cff')
-process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
-process.gtDigis.UnpackBxInEvent = cms.int32(3)
+process.load('Configuration.StandardSequences.RawToDigi_cff')
+#process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
 process.load('Configuration.StandardSequences.Reconstruction_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = 'GR_R_311_V2::All'
-#process.GlobalTag.globaltag = 'START311_V2::All'
-#process.GlobalTag.globaltag = 'DESIGN311_V1::All'
-#process.GlobalTag.globaltag = 'MC_311_V2::All'
-#process.GlobalTag.globaltag = 'MC_39Y_V7::All'
+process.GlobalTag.globaltag = 'MC_42_V12::All'
 
 #
 # privare rpc r2d
@@ -59,7 +45,7 @@ process.l1RpcEmulDigis.nBxToProcess = cms.int32(5)
 
 
 #
-# mesage logger
+# message logger
 #
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.MessageLogger = cms.Service("MessageLogger",
@@ -95,6 +81,12 @@ process.load("DQM.RPCMonitorClient.RPCMonitorRaw_cfi")
 process.rpcMonitorRaw.writeHistograms = cms.untracked.bool(True)
 process.rpcMonitorRaw.histoFileName = cms.untracked.string("rawMonitor.root")
 
+# process.load("RecoMuon.GlobalTrackingTools.GlobalMuonRefitter_cff")
+process.load("RecoMuon.GlobalTrackingTools.GlobalTrackQuality_cfi")
+process.load("RecoMuon.MuonIdentification.refitMuons_cfi")
+process.load("TrackingTools.TrackRefitter.globalMuonTrajectories_cff")
+
+
 
 #process.filterBX = cms.EDFilter("FilterBX", beamBX = cms.vuint32(1,100) )
 
@@ -111,7 +103,7 @@ process.linkSynchroAnalysis =  cms.EDAnalyzer("LinkSynchroAnalysis",
   writeHistograms = cms.untracked.bool(True),
   histoFileName = cms.untracked.string("synchroAnalysis.root"),
   linkMonitorPSet = cms.PSet(
-    useFirstHitOnly = cms.untracked.bool(False),
+    useFirstHitOnly = cms.untracked.bool(True),
     dumpDelays = cms.untracked.bool(False)
   ),
 #  synchroSelectorL1Muon = cms.PSet(
@@ -152,6 +144,7 @@ process.efficiencyTree = cms.EDAnalyzer("EfficiencyTree",
   maxTIP = cms.double(0.5),
   maxEta = cms.double(1.55),
   l1MuReadout = cms.InputTag("gtDigis"),
+  l1MuReadoutEmu = cms.InputTag("gtDigis"),
   rpcMatcherPSet =  cms.PSet( maxDeltaEta = cms.double(0.5), maxDeltaPhi = cms.double(0.5)), 
   dtcscMatcherPSet = cms.PSet( maxDeltaEta = cms.double(0.1), maxDeltaPhi = cms.double(0.1))
 )
@@ -162,12 +155,13 @@ process.p = cms.Path(
 #  process.filterBX*
 #  process.primaryVertexFilter*
   process.gtDigis*
-  process.filterL1* 
-  process.muonRPCDigis*
-  process.l1RpcEmulDigis*
-  process.rpcFEDIntegrity*process.rpcMonitorRaw*
-  process.l1compare*process.l1demon*
-  process.l1trpctf*
-  process.linkSynchroAnalysis
+#  process.filterL1* 
+#  process.muonRPCDigis*
+#  process.l1RpcEmulDigis*
+#  process.rpcFEDIntegrity*process.rpcMonitorRaw*
+#  process.l1compare*process.l1demon*
+#  process.l1trpctf*
+#  process.linkSynchroAnalysis
+  process.globalMuons*
+  process.efficiencyTree
   )
-#  process.efficiencyTree)
